df9f0ed55c6e09b59a14aef33d57f6a7ac89f82c
Prafful Kumar <84987149+praffulkumar26@users.noreply.github.com>
2022-11-27 22:35:17 +0530

    Add files via upload

---
+++ mdps.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 22:23:09 2022
+
+@author: praff
+"""
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+#loading the saved models
+
+diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_model.sav', 'rb'))
+
+#sidebar for navigate
+
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction System',
+                           
+                           ['Diabetes Prediction',
+                            'Heart Prediction'],
+                           
+                           icons=['activity', 'heart'],
+                           
+                           default_index=0)
+    
+# diabetes prediction page
+if(selected == 'Diabetes Prediction'):
+    
+    #page title
+    st.title('Diabetes Prediction Using ML')
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        Pregnancies = st.text_input('Number of Pregnancies')
+        
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+        
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure Value')
+        
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness Value')
+        
+    with col2:
+        Insulin = st.text_input('Insulin Level')
+        
+    with col3:
+        BMI = st.text_input('BMI Value')
+        
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function Value')
+        
+    with col2:
+        Age = st.text_input('Age of the Person')
+            
+    
+    #code for prediction
+    diab_diagnosis = ''
+    
+    #creating a button for prediction
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        
+        if(diab_prediction[0]==1):
+            diab_diagnosis = 'The person is Diabetic'
+        
+        else:
+            diab_diagnosis = 'The person is not Diabetic'
+    
+    st.success(diab_diagnosis)
+
+#Heart Disease Prediction Page
+if (selected == 'Heart Prediction'):
+    
+    
+    #page title
+    st.title('Heart Prediction Using ML')
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        sex = st.text_input('Sex')
+        
+    with col3:
+        cp = st.text_input('Chest Pain Type')
+        
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+        
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+        
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 md/dl')
+        
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic result')
+        
+    with col2:
+        thalach	 = st.text_input('Maximum Heart Rate Achieved')
+            
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+        
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+        
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+        
+    with col3:
+        ca = st.text_input('Major vessels colored by fourosopy')
+        
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+    
+    #code for prediction
+    heart_diagnosis = ''
+    
+    #creating a button for prediction
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+        
+        if(heart_prediction[0]==1):
+            heart_diagnosis = 'The person is having heart disease'
+        
+        else:
+            heart_diagnosis = 'The person is not having heart disease'
+    
+    st.success(heart_diagnosis)
